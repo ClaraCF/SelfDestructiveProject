b76728821ceef3f8909712b6f5c7e282b806902c
clara <claracarrasquelfiorini@gmail.com>
2021-07-12 12:43:05 -0500

    Add multimedia playing features

--- src/window.py
+++ src/window.py
@@ -1,5 +1,9 @@
+from PIL import Image as Img
+import simpleaudio as sa
+from PIL import ImageTk
 from tkinter import *
 import tkvideo
+import cv2
 
 
 class Window(Tk):
@@ -12,11 +16,39 @@ class Window(Tk):
 
         self.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
-    def play_video(self, path: str):
-        holder = Label(self)
-        player = tkvideo.tkvideo(path, holder, loop=0, size=(self.winfo_height(), self.winfo_width()))
+    @staticmethod
+    def play_sound(path):
+        return sa.WaveObject.from_wave_file(path).play()
 
+    def play_video(self, filename: str):
+        # Create a holder for the video
+        holder = Frame(self)
         holder.pack(side=TOP, expand=YES)
+
+        # Create the video player
+        player = tkvideo.tkvideo(filename, holder, loop=0, size=(self.winfo_width(), self.winfo_height()))
         player.play()
 
-        return holder
+        # Calculate the duration of the video
+        video = cv2.VideoCapture(filename)
+        fps = video.get(cv2.CAP_PROP_FPS)
+        frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        duration = int(frame_count / fps) * 1000
+        video.release()
+
+        # Start playing the video's sound
+        audio = self.play_sound(filename)
+
+        # Schedule frame for deletion and audio for stopping when video ends
+        self.after(duration, func=lambda: [holder.destroy(), audio.stop()])
+
+    def display_image(self, filename, duration=0):
+        image = ImageTk.PhotoImage(Img.open(filename))
+        image_holder = Label(self, image=image)
+        image_holder.pack(side=TOP, expand=YES)
+        self.update()
+
+        if duration:
+            self.after(duration, func=image_holder.destroy)
+
+        return image_holder
