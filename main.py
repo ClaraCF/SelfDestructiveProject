add0a825e55a7df4ca5048ef888113911cd98cc7
clara <claracarrasquelfiorini@gmail.com>
2021-07-14 20:18:53 -0500

    Implemented functions from src in main.py

--- main.py
+++ main.py
@@ -1,98 +1,70 @@
-from PIL import Image as Img, ImageTk
-from pydub.playback import play
+from src.process import Background
+from src.file_utils import *
+from src.window import *
+
 from num2words import num2words
-from pydub import AudioSegment
 from tkinter import *
-import tkvideo
-import time
+import subprocess
 import sys
 import os
 
-
-suicide_count = 6
-
+# Keep track of the amount of times the project has died and recovered
+suicide_count = 1
 PARENT_DIR = sys.path[0]
 
-window = Tk()
+
+window = Window()
 window.title(f"Suicide Bomber The {num2words(suicide_count, to='ordinal').capitalize()}")
-
-width = 900
-height = 500
-
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-
-x = (screen_width / 2) - (width / 2)
-y = (screen_height / 2) - (height / 2)
-
-window.geometry('%dx%d+%d+%d' % (width, height, x, y))
+window.center()
 
 
-rebibis = None
-rebibis_holder = None
+# Let us commence forth
+def CommitSuicide():
+    # First backup all files in memory
+    backup_thread = Background(target=Backup, args=(PARENT_DIR,))
+    backup_thread.start()
+
+    # Show audiovisual assets
+    escubi = f"{PARENT_DIR}/res/escubi.mp3"
+    explosion = f"{PARENT_DIR}/res/explosion.mp4"
+    rebibis = f"{PARENT_DIR}/res/rebibis.jpg"
+
+    # Display shaggy's image and play sound
+    rebibis = window.display_image(rebibis)
+    window.play_sound(escubi).wait_done()
+    rebibis.destroy()
+
+    # Play explosion video
+    window.play_video(explosion)
+
+    # Close the window
+    window.destroy()
+    
+    # Wait for the thread to finish backing up files
+    if backup_thread.is_alive():
+        backup_thread.join()
+
+    # Aquire the backed up project as a dict
+    project = backup_thread.get_return_value()
+
+    # Increase the suicide count
+    main = f"{PARENT_DIR}/main.py"
+    source: str = project[main].decode("utf-8")
+    source = source.replace(f"suicide_count = {suicide_count}", f"suicide_count = {suicide_count + 1}")
+    project[main] = source.encode("utf-8")
+
+    # Call the restoration script
+    # restore.py <pid> <time <project> <directory> <relaunch> [path]
+    args = [os.getpid(), 7, project, PARENT_DIR, True, f"{PARENT_DIR}/main.py"]
+    subprocess.Popen((["python3", f"{PARENT_DIR}/src/restore.py"] + args))
+
+    # Then start recurively deleting the project's files
+    Obliterate(PARENT_DIR)
 
 
-def PlayShaggy():
-    global PARENT_DIR
-    global window
-    global rebibis
-    global rebibis_holder
-
-    rebibis = ImageTk.PhotoImage(Img.open(f"{PARENT_DIR}/res/rebibis.jpg"))
-    rebibis_holder = Label(window, image=rebibis)
-    rebibis_holder.pack(side=TOP, expand=YES)
-
-    play(AudioSegment.from_mp3(f"{PARENT_DIR}/res/escubi.mp3"))
-    rebibis_holder.destroy()
-    window.update()
-
-
-holder = Label(window)
-player = tkvideo.tkvideo(f"{PARENT_DIR}/res/explosion.mp4", holder, loop=0, size=(int(width), int(height)))
-
-
-def Kamikaze():
-    global PARENT_DIR
-    global window
-    global holder
-    global player
-
-    holder.pack(side=TOP, expand=YES)
-    player.play()
-
-    window.update()
-
-
-def Magic():
-    global PARENT_DIR
-
-    source_path = f"{PARENT_DIR}/fuaLaRebibis_{suicide_count}.py"
-    escubi_mp3_path = f"{PARENT_DIR}/res/escubi.mp3"
-    explosion_mp4_path = f"{PARENT_DIR}/res/explosion.mp4"
-    rebibis_jpg_path = f"{PARENT_DIR}/res/rebibis.jpg"
-    paths = [source_path, escubi_mp3_path, explosion_mp4_path, rebibis_jpg_path]
-
-    source = Backup(source_path).decode("utf-8")
-    source = source.replace(f"suicide_count = {suicide_count}", f"suicide_count = {suicide_count + 1}").encode("utf-8")
-
-    escubi_mp3 = Backup(escubi_mp3_path)
-    explosion_mp4 = Backup(explosion_mp4_path)
-    rebibis_jpg = Backup(rebibis_jpg_path)
-    backups = (source, escubi_mp3, explosion_mp4, rebibis_jpg)
-
-    os.system("rm -rf ./*")
-    time.sleep(7)
-    os.system("mkdir res")
-    paths[0] = source_path.replace(str(suicide_count), str(suicide_count + 1))
-    paths = tuple(paths)
-
-    for i, j in zip(paths, backups):
-        Restore(i, j)
-
-
-button = Button(window, text="No me presiones")
-
-button["command"] = lambda: [button.destroy(), PlayShaggy(), Kamikaze(), window.destroy()]  # , Magic()]
+# Bri'ish?!
+button = Button(window, text="Don't click me")
+button["command"] = lambda: [button.destroy(), CommitSuicide()]
 button.pack(side=TOP, expand=YES)
 
 
